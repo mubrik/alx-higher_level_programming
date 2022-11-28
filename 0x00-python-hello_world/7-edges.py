f9774dfdbfe50e61860865d6f769c8dd73a0c0f9
mubrik <mubarakg4u@gmail.com>
2022-11-28 14:16:36 +0100

    first

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+print(f"First 3 letters: {word[:3]}")
+print(f"Last 2 letters: {word[-2:]}")
+print(f"Middle word: {word[1:len(word) -1]}")
+# making this 8 lines long for requirement
+# making this 8 lines long for requirement
+# making this 8 lines long for requirement
