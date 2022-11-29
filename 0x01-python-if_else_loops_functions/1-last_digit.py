8af2e463ebe867f0890131e9d2e5a957fbb87bd0
mubrik <mubarakg4u@gmail.com>
2022-11-29 16:11:18 +0100

    2 format fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,7 @@ import random
 number = random.randint(-10000, 10000)
 is_neg = number < 0
 last_d = (abs(number) % 10)
-""" txt = "and is 0" if last_d == 0 else "and is greater than 5" if\
-  not is_neg and last_d > 5 else "and is less than 6 and not 0" """
+
 # text
 txt = None
 if last_d == 0:
