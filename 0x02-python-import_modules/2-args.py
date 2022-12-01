a4da719e0b5e7f44eaab93dead4103cb8ca994ad
mubrik <mubarakg4u@gmail.com>
2022-12-01 15:45:03 +0100

    Commited from Script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,10 @@ import sys
 
 def main():
     argc = len(sys.argv)
-    print("{:d} argument{}".format(argc - 1, "." if argc == 1 else "s:"))
+    s = "argument"
+    if argc == 1 or argc > 2:
+        s = "arguments"
+    print("{:d} {}{}".format(argc - 1, s, "." if argc == 1 else ":"))
     for i, arg in enumerate(sys.argv):
         if i == 0:
             continue
