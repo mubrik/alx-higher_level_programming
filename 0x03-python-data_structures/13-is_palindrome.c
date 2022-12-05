b657493398d2fdc6cb93aada0ad55379a4b1aae0
mubrik <mubarakg4u@gmail.com>
2022-12-05 18:54:20 +0100

    Commited from Script

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,7 +9,7 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *tmp;
-	int list_s = 0, list_half = 0, i = 0, j = 0, is_odd = 0, c = 0, *num_list;
+	int list_s = 0, list_half = 0, i = 0, j = 0, c = 0, *num_list;
 
 	if (!head)
 		return (1);
@@ -21,8 +21,6 @@ int is_palindrome(listint_t **head)
 	if (list_s == 1)
 		return (1);
 	list_half = list_s / 2;
-	if (list_s % 2 != 0)
-		list_half = (list_s - 1) / 2, is_odd = 1;
 	/* creat list, only half sizzed for space efficiency */
 	num_list = malloc(sizeof(int) * list_half);
 	if (!num_list)
@@ -32,7 +30,7 @@ int is_palindrome(listint_t **head)
 	/* fill up mallocd list from middway of linked list */
 	for (i = 0; tmp; i++)
 	{
-		c = is_odd ? list_half : list_half - 1;
+		c = list_s % 2 != 0 ? list_half : list_half - 1;
 		if (i > c)
 			num_list[j] = tmp->n, j++;
 		tmp = tmp->next;
