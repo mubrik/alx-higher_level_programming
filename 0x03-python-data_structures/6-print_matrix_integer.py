daccdc56871cab264d3311eeed9b0961093a5c80
mubrik <mubarakg4u@gmail.com>
2022-12-05 22:59:23 +0100

    Commited from Script

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,4 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for j, col in enumerate(row):
             print("{}{:d}".format(" " if j != 0 else "", col), end="")
-            if j == len(row) - 1:
-                print()  # new line
+        print()  # new line
