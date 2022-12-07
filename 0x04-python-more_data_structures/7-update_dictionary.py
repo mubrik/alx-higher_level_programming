7f911ffbf915f499ffe3d12fd8c59693afa811aa
mubrik <mubarakg4u@gmail.com>
2022-12-07 13:54:40 +0100

    Commited from Script

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -5,5 +5,6 @@ def update_dictionary(a_dictionary, key, value):
     """ update a dictionary, what a weird func """
     if not a_dictionary:
         return a_dictionary
-    a_dictionary[key] = value
+    if key:
+        a_dictionary[key] = value
     return a_dictionary
