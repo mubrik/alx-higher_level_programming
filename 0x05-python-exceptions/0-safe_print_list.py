8aba48e73639a338c32177447e296091d1eda4ec
mubrik <mubarakg4u@gmail.com>
2022-12-19 13:46:04 +0100

    Commited from Script

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,12 +8,10 @@ def safe_print_list(my_list=[], x=0):
         print()
         return count
     for index in range(x):
-        """ if index >= x:
-            break """
         try:
             print(my_list[index], end="")
             count += 1
-        except Exception:
+        except IndexError:
             break
     print()
     return count
