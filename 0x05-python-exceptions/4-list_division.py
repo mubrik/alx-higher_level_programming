5b7599fbd9cf9931e59e0e26be20d4f4a4ca9f2c
mubrik <mubarakg4u@gmail.com>
2022-12-19 14:00:27 +0100

    Commited from Script

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,13 +3,15 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     """Write a function that divides element by element 2 lists."""
-    if not my_list_1 or not my_list_2:
+    if not my_list_1 or not my_list_2 or not list_length:
         return []
     result = 0
     new_list = [0 for num in range(list_length)]
     for index in range(list_length):
         try:
             a, b = my_list_1[index], my_list_2[index]
+            if not isinstance(a, int) or not isinstance(b, int):
+                raise TypeError
             result = a / b
         except (TypeError, IndexError, ZeroDivisionError, Exception) as ex:
             ty = type(ex)
