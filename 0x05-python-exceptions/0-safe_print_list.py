762ea503fae8435c980a95f8ae3037a282937d53
mubrik <mubarakg4u@gmail.com>
2022-12-19 13:37:11 +0100

    Commited from Script

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@
 
 def safe_print_list(my_list=[], x=0):
     """Write a function that prints x elements of a list."""
-    if not my_list:
-        return
     count = 0
+    if not my_list:
+        return count
     for index, item in enumerate(my_list):
         if index >= x:
             break
