3a89c5dc4ef8bbabbc29bfdbb03349637019e79c
mubrik <mubarakg4u@gmail.com>
2022-12-20 15:35:37 +0100

    Commited from Script

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -10,7 +10,7 @@ class MagicClass:
     def __init__(self, radius):
         """ magic method for init """
         self.__radius = 0
-        if (type(radius) != int) or (type(radius) != float):
+        if (type(radius) is not int) or (type(radius) is not float):
             raise TypeError("radius must be a number")
         else:
             self.__radius = radius
