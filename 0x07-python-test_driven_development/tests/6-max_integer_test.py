4474bbd2c6107717b537d18747ad76deb2b6de99
mubrik <mubarakg4u@gmail.com>
2023-01-04 21:26:38 +0100

    Commited from Script

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,9 +11,33 @@ class TestMaxInteger(unittest.TestCase):
         result = max_integer([])
         self.assertIs(result, None)
 
+    def test_one_num(self):
+        result = max_integer([5])
+        self.assertEqual(result, 5)
+
     def test_all_equal(self):
         result = max_integer([5, 5, 5, 5])
         self.assertEqual(result, 5)
+
+    def test_max_start(self):
+        result = max_integer([10, 5, 5, 5])
+        self.assertEqual(result, 10)
+
+    def test_max_end(self):
+        result = max_integer([10, 5, 5, 21])
+        self.assertEqual(result, 21)
+
+    def test_max_middle(self):
+        result = max_integer([10, 5, 50, 5, 21])
+        self.assertEqual(result, 50)
+
+    def test_one_neg(self):
+        result = max_integer([10, -1, 50, 5, 21])
+        self.assertEqual(result, 50)
+
+    def test_all_neg(self):
+        result = max_integer([-10, -1, -50, -5, -21])
+        self.assertEqual(result, -1)
 
 
 if __name__ == '__main__':
