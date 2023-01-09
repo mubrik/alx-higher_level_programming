de528259ba5788b90042d9cb35c4b4371cfee818
mubrik <mubarakg4u@gmail.com>
2023-01-09 17:54:44 +0100

    Commited from Script

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -14,7 +14,7 @@ class BaseGeometry:
         """ if not isinstance(name, str):
             raise TypeError("name must be a string") """
         if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
         return value
