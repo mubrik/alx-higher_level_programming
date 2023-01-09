245e09199c538d9eb229ee154b7dc10589f6741f
mubrik <mubarakg4u@gmail.com>
2023-01-09 18:41:06 +0100

    Commited from Script

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,17 +3,17 @@
 
 
 class BaseGeometry:
-    """ BaseGeometry class """
+    """ Main BaseGeometry class """
 
     def area(self):
         """ area of geometry """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ validate value """
+        """ validate value to be an int """
         if not isinstance(name, str):
             raise TypeError("name must be a string")
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
