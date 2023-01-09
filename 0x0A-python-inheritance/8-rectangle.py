ac1f070a6086ebc2b8baeaf5753053c2ba5f980d
mubrik <mubarakg4u@gmail.com>
2023-01-09 19:05:15 +0100

    Commited from Script

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -11,12 +11,11 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """ validate value to be an int """
-        if not isinstance(name, str):
-            raise TypeError("name must be a string")
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+        return value
 
 
 class Rectangle(BaseGeometry):
@@ -24,7 +23,5 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """ init method """
-        super().integer_validator("height", height)
-        super().integer_validator("width", width)
-        self.__height = height
-        self.__width = width
+        self.__height = super().integer_validator("height", height)
+        self.__width = super().integer_validator("width", width)
