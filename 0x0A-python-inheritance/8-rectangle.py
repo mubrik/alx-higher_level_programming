4a5d2fdce41b5944a81b60d3cea3555003c64d54
mubrik <mubarakg4u@gmail.com>
2023-01-09 18:42:44 +0100

    Commited from Script

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -17,7 +17,6 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        return value
 
 
 class Rectangle(BaseGeometry):
