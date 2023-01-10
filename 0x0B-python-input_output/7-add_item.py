28dd9b4767a26471116aafb290bd0df9289a69e8
mubrik <mubarakg4u@gmail.com>
2023-01-10 17:31:53 +0100

    Commited from Script

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Working with Files and JSON """
+""" Working with Files and JSON  """
 import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
