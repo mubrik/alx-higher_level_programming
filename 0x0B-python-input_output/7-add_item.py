388615fb76b808a1b588e72edeb75ab84e635d1e
mubrik <mubarakg4u@gmail.com>
2023-01-10 17:10:20 +0100

    Commited from Script

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,22 +7,12 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 def main():
     """ store input args in JSON obj """
-    # make sure file exists
     try:
-        with open("add_item.json", "x", encoding="utf-8"):
-            pass
-    except FileExistsError as e:
-        pass
-    finally:
-        # file exist, make sure line valid json obj
-        with open("add_item.json", "r+") as f:
-            if len(f.readline()) == 0:
-                f.write('[]')
-        # file exist, contains valid json
-        if len(sys.argv) == 1:
-            return
         # get obj
         arr = load_from_json_file("add_item.json")
+    except FileNotFoundError as e:
+        arr = []
+    finally:
         for i, arg in enumerate(sys.argv):
             if i == 0:
                 continue
