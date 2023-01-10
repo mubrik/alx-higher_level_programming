601f778c3e1780ecb7b61288f3786628314475b8
mubrik <mubarakg4u@gmail.com>
2023-01-10 17:14:28 +0100

    Commited from Script

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-""" Working with Files and JSON """
-import sys
+"""
+A script that takes all the arguments given to a file and saved it as a list in
+json format.
+"""
+
+from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-def on_lod():
-    """ store input args in JSON obj """
+def run():
+    """Stores the arguments"""
+    del argv[0]
     try:
-        # get obj
-        arr = load_from_json_file("add_item.json")
-    except FileNotFoundError as e:
-        arr = []
-    finally:
-        for i, arg in enumerate(sys.argv):
-            if i == 0:
-                continue
-            arr.append(arg)
-        # save array
-        save_to_json_file(arr, "add_item.json")
+        argList = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        argList = []
+
+    argList += argv
+    save_to_json_file(argList, 'add_item.json')
 
 
 if __name__ == "__main__":
-    on_lod()
+    run()
