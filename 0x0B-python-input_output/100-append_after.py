897afea66af587de2418ff72783319c0e07b0a47
mubrik <mubarakg4u@gmail.com>
2023-01-10 22:58:41 +0100

    Commited from Script

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Write File """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ apend after a search str """
+    new_str = []
+    with open(filename, "r", encoding="utf-8") as f:
+        # get line and add each to list list
+        for line in f:
+            print(line)
+            new_str.append(line)
+            if line.find(search_string) >= 0:
+                new_str.append(new_string)
+    # truncate and fill
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write("".join(new_str))
