2e144e55ddce5d4142a7da51cdf330a26623cc74
mubrik <mubarakg4u@gmail.com>
2023-01-10 23:06:50 +0100

    Commited from Script

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,11 +4,14 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """ apend after a search str """
+    if not filename or not isinstance(filename, str):
+        return
+    if not isinstance(search_string, str) or not isinstance(new_string, str):
+        return
     new_str = []
     with open(filename, "r", encoding="utf-8") as f:
         # get line and add each to list list
         for line in f:
-            print(line)
             new_str.append(line)
             if line.find(search_string) >= 0:
                 new_str.append(new_string)
