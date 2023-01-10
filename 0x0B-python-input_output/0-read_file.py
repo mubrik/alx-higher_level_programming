19202f4765ac6b0123285ead57d17129f78cf70f
mubrik <mubarakg4u@gmail.com>
2023-01-10 15:48:49 +0100

    Commited from Script

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,8 +4,5 @@
 
 def read_file(filename=""):
     """ reads a file and prints to stdout """
-    try:
-        with open(filename, "r", encoding="utf-8") as f:
-            print(f.read(), end="")
-    except OSError as e:
-        print("File cant be opened")
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,10 +4,7 @@
 
 def write_file(filename="", text=""):
     """ Write to a file, create if not exist, truncate if exist """
-    try:
-        char_w = 0
-        with open(filename, "w", encoding="utf-8") as f:
-            char_w = f.write(text)
-        return char_w
-    except OSError as e:
-        print("File cant be opened")
+    char_w = 0
+    with open(filename, "w", encoding="utf-8") as f:
+        char_w = f.write(text)
+    return char_w
