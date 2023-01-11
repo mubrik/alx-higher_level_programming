172892b8f5eb6678637526c1c135cf255639e81b
mubrik <mubarakg4u@gmail.com>
2023-01-11 18:22:41 +0100

    Commited from Script

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,7 +4,7 @@ import fileinput
 
 
 def print_out(pr_num, pr_arr):
-    print(f"File size: {pr_num}")
+    print("File size: {}".format(pr_num))
     # filter and sort
     pr_arr = sorted(filter(lambda x: x[1], pr_arr))
     arr = [f'{item[0]}: {item[1]}' for item in pr_arr]
