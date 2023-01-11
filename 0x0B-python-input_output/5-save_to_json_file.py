dd671fa1b8a52bc2b054fa80531389e7beeaf48d
mubrik <mubarakg4u@gmail.com>
2023-01-11 18:18:22 +0100

    Commited from Script

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ Working with Files and JSON """
 import json
-write_file = __import__("1-write_file").write_file
 
 
 def save_to_json_file(my_obj, filename):
     """ save obj JSON to file """
-    return write_file(filename, json.dumps(my_obj))
+    char_w = 0
+    with open(filename, "w", encoding="utf-8") as f:
+        char_w = f.write(json.dumps(my_obj))
+    return char_w
