d691f88ec35181fb117661271e4e4c15ad5550f0
mubrik <mubarakg4u@gmail.com>
2023-03-14 00:45:22 +0100

    Commited from Script

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,12 +1,12 @@
-#!/usr/bin/node
+#!/home/mubrik/.nvm/versions/node/v19.2.0/bin/node
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
 myObject.incr = function () {
-  this.value = ++this.value;
-};
+  this.value = this.value + 1;
+}
 myObject.incr();
 console.log(myObject);
 myObject.incr();
