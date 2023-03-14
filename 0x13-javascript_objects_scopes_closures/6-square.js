9a072628dff03710169811dbec4dcfbcc3e71cb4
mubrik <mubarakg4u@gmail.com>
2023-03-14 18:54:32 +0100

    Commited from Script

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const SquareA = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends SquareA {
   constructor (size) {
     super(size, size);
   }
 
   charPrint (char) {
-    const _char = char ? char : "X";
+    const _char = char ?? 'X';
     for (let index = 0; index < this.height; index++) {
       console.log(_char.repeat(this.width));
     }
