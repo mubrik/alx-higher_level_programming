3dd6f616f781fe826a4a8a8d21179798e13ceb84
mubrik <mubarakg4u@gmail.com>
2023-03-17 15:11:44 +0100

    Commited from Script

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,23 +8,24 @@ import MySQLdb
 
 def main():
     '''main func'''
-    if (sys.argv.__len__() != 4):
+    if (sys.argv.__len__() != 5):
         return
-    _, username, password, dbname = sys.argv
+    _, username, password, dbname, state = sys.argv
 
     try:
         db = MySQLdb.connect(host="localhost", port=3306, user=username,
                              password=password, database=dbname)
         cursor = db.cursor()
-        cursor.execute(
-            """SELECT id, name FROM states
-            WHERE name LIKE 'N%'
-            ORDER BY id ASC""")
+        cursor.execute("""SELECT cities.name
+                       FROM states
+                       RIGHT JOIN cities ON cities.state_id=states.id
+                       WHERE states.name=%s
+                       ORDER BY cities.id""",
+                       [state])
+        res = ", ".join(list(map(lambda x: x[0], cursor.fetchall())))
+        print(res)
     except MySQLdb.MySQLError as exc:
         print("DB Error", exc)
-
-    for row in cursor.fetchall():
-        print(row)
 
 
 if __name__ == '__main__':
