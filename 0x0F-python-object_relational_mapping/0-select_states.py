32c7dd88fe22efd4aec036828eb8cfb81509d9ca
mubrik <mubarakg4u@gmail.com>
2023-03-17 10:38:05 +0100

    Commited from Script

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+''' script that lists all states from the database hbtn_0e_0_usa '''
+
+import sys
+import MySQLdb
+
+
+def main():
+    '''main func'''
+    if (sys.argv.__len__() != 4):
+        return
+    _, username, password, dbname = sys.argv
+
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                             password=password, database=dbname)
+        cursor = db.cursor()
+        cursor.execute("""SELECT id, name FROM states ORDER by id ASC""")
+    except MySQLdb.MySQLError as exc:
+        print("DB Error", exc)
+
+    for row in cursor.fetchall():
+        print(row)
+
+
+if __name__ == '__main__':
+    main()
