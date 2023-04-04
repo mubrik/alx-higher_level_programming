2efafa981db7e191589c94d1d518f5ab8dcaa19c
mubrik <mubarakg4u@gmail.com>
2023-04-04 10:27:02 +0100

    Commited from Script

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # check body of 200 req
-curl -s -w '\n%{http_code}\n' "$1" | awk 'END {if ($0 ~ /200/) {show_body=1};} /^$/ {body=1;next} {if (show_body && body) print $0}'
+curl -s --location "$1"
