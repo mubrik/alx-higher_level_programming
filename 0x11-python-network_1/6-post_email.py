a544b66a50d25c68919d4029fc309c21804fe34a
mubrik <mubarakg4u@gmail.com>
2023-04-04 15:28:07 +0100

    Commited from Script

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 """ gets alx status with urllib"""
 import sys
-import urllib
-import urllib.request
+import requests
 
 
 def main(url, email):
     """ main script"""
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req = urllib.request.Request(url, data=data, method='POST')
-
-    with urllib.request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
-        print(body)
+    response = requests.post(url, data={'email': email})
+    print(response.text)
 
 
-if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        url, email = sys.argv
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        _, url, email = sys.argv
         main(url, email)
